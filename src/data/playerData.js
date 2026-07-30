3cbdac3cc6fb5fa03960de0e6bc19556793f9988
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-07-30 20:12:04 +0300

    simulating dynamic data

---
+++ src/data/playerData.js
@@ -0,0 +1,65 @@
+const playerData = [
+  {
+    id: 1,
+    name: "Ken Wachira",
+    role: "Basketball Coach",
+    about: `Meet Ken Wachira, a basketball strategist known for his quick decision-making and game-reading abilities. His coaching style emphasizes enhancing court vision and predicting opponents' moves. Gain exclusive insights into basketball tactics and honing your 3-point game with Ken Wachira, the maestro behind the arc.'`,
+    tag: "Read the game, own the court.",
+    image:
+      "https://content.api.news/v3/images/bin/866c2c98b97f0f728664e4f849c57bed",
+    attributes: [
+      {
+        title: "Desicive moves specialist",
+        stats: [
+          {
+            title: "Strength",
+            stat: [
+              "Strategic Vision & Quick Decision-Making & 3-Point Shooting",
+            ],
+          },
+          {
+            title: "Expertise",
+            stat: [
+              "Reading the game, making decisive moves, excellent 3-point shooting.",
+            ],
+          },
+        ],
+      },
+      {
+        title: "Visionary insights",
+        stats: [
+          {
+            title: "Coaching Focus",
+            stat: ["Improving court vision, anticipating opponent moves"],
+          },
+          {
+            title: "Main focus",
+            stat: ["Tactical insights", "Rapid decision-making drills"],
+          },
+        ],
+      },
+      {
+        title: "Training positions",
+        stats: [
+          {
+            title: "Main focus",
+            stat: ["Point guards", "Small forwards", "Team strategists"],
+          },
+          {
+            title: "Also",
+            stat: ["Tactical insights"],
+          },
+        ],
+      },
+      {
+        title: "Accolades",
+        stats: [
+          {
+            title: "Mvp 2022",
+            stat: ["renowned for exceptional 3-point shooting skills."],
+          },
+        ],
+      },
+    ],
+  },
+];
