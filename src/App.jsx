09b79344a4db1a8c49c6f8710e7fb32d52f26af2
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-07-30 17:25:04 +0300

    created profile component and related styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import styles from "./App.module.css";
 
 import PlayerAttributes from "./components/PlayerAttributes";
+import PlayerProfile from "./components/PlayerProfile";
 
 function App() {
   return (
     <div className={styles.profile}>
+      <PlayerProfile />
       <PlayerAttributes />
     </div>
   );
---
+++ src/components/PlayerProfile.jsx
@@ -0,0 +1,7 @@
+import styles from "./PlayerProfile.module.css";
+
+function PlayerProfile() {
+  return <div className={styles.playerProfile}></div>;
+}
+
+export default PlayerProfile;
