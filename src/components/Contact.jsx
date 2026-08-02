6a5324f4d970cb6f979a98fdb077def9e35517cf
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-08-02 10:11:11 +0300

    added event handler to CTA

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,14 @@
 import styles from "./Contact.module.css";
 function Contact() {
+  function handleClick(e) {
+    console.log("You have interacted with the profile");
+  }
   return (
     <div className={styles.contact}>
-      <button>Contact</button>
-      <button className={styles.share}>Share</button>
+      <button onClick={handleClick}>Contact</button>
+      <button className={styles.share} onClick={handleClick}>
+        Share
+      </button>
     </div>
   );
 }
